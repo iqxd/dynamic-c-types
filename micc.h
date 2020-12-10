3488be8eb22c48cbd06a5bb44ca767bf1ca59d1e
iqxd <327614680@qq.com>
2020-12-10 20:35:46 +0800

    create some basic data structures

---
+++ micc.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <assert.h>
+
+#define _CRT_SECURE_NO_WARNINGS
+
+typedef int64_t int_t;
+typedef uint64_t uint_t;
+#define INT_MIN  (-9223372036854775807i64 - 1)
+#define INT_MAX  9223372036854775807i64
+#define UINT_MAX 0xffffffffffffffffui64
+
---
+++ var.h
@@ -0,0 +1,51 @@
+#include "micc.h"
+
+typedef struct _type type_t;
+
+typedef enum
+{
+    NONE,
+    INT,
+    UINT,
+    FLOAT,
+    STR,
+    SSTR,
+    LIST,
+    DICT
+} type_id;
+
+typedef struct {
+    type_t *type;
+} var_t;
+
+struct _type {
+    type_id id;
+    void (*print_func)(var_t*);
+};
+
+typedef struct {
+    type_t *type;
+    union {
+        int_t ival;
+        uint_t uval;
+        double fval;
+    };
+} num_t;
+
+typedef struct {
+    type_t *type;
+    union {
+        char sval[16];
+        struct {
+            size_t len;
+            char *sref;
+        };
+    };
+} str_t;
+
+typedef struct {
+    type_t *type;
+    size_t len;
+    size_t capacity;
+    var_t *lref;
+} list_t;
