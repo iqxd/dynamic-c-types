f05d580039f4848dab3ee96948c624d6365e0622
iqxd <327614680@qq.com>
2020-12-28 20:06:35 +0800

    codes update

--- tagptr.h
+++ tagptr.h
@@ -1,5 +1,21 @@
 #pragma once
-#include "config.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <assert.h>
+
+#ifdef _MSC_VER
+    #define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#define HEAP_OBJECT_BYTES 24
+#define SHORT_STR_ALLOC_BYTES 23
+#define LIST_RESERVED_ELEMS 10
+#define LIST_INCR_FACTOR 1.8
+#define DICT_KEY_ALLOC_BYTES 24
+#define DICT_RESERVED_NODES 16
 
 typedef uint64_t tagptr_t;
 #define TAG_ALLOC_BITS 6
